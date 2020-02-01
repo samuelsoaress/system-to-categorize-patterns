476e880bd1de753b63c0da6aed10f155e8f499cc
getsam <samuelsoares177778@gmail.com>
2020-01-31 23:59:25 -0300

    modificações no servidor

--- servidor.py
+++ servidor.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, redirect, flash, send_from_directory
+from flask import Flask, render_template, request, redirect, flash, send_from_directory, url_for
 import os, time
 
 app = Flask(__name__)
 
 app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 
+nome_df = ""
+
 @app.route('/')
 def index():
     return render_template('index.html', titulo='Suba seu Dataset')
 
-
+@app.route('/upload',methods=['POST'])
+def upload():
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/{arquivo.filename}')
+    #nome_df = arquivo.filename
+    return redirect(url_for('index'))
 app.run(debug=True)
